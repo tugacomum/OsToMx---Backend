c8e00b8cc71a101afdb1f347c93ee300c0814f95
AzeezOn <pv25942@alunos.estgv.ipv.pt>
2024-03-13 16:56:19 +0000

    teste

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.executeScript = void 0;
 const mendixmodelsdk_1 = require("mendixmodelsdk");
 const mendixplatformsdk_1 = require("mendixplatformsdk");
-const APP_ID = "f5809059-4962-4d45-ba11-bc8de7f1bbc7";
+const APP_ID = "15e89864-5ea4-4f36-ab1c-cebf70d4944b";
 async function executeScript(jsonData) {
     await getApp(APP_ID);
     async function getApp(ai) {
@@ -18,32 +18,70 @@ async function executeScript(jsonData) {
             .allDomainModels()
             .filter((dm) => dm.containerAsModule.name === "MyFirstModule")[0];
         const domainModel = await domainModelInterface.load();
-        for (const entityData of jsonData) {
-            const entityName = entityData.Name;
-            const entity = mendixmodelsdk_1.domainmodels.Entity.createIn(domainModel);
-            entity.name = entityName;
-            for (const attributeData of entityData.Attributes) {
-                const attributeName = attributeData.Attribute;
-                if (attributeName != "Id") {
-                    const attributeType = getDataType(attributeData.Type);
-                    const attribute = mendixmodelsdk_1.domainmodels.Attribute.createIn(entity);
-                    attribute.name = attributeName;
-                    attribute.type = attributeType;
-                    if (attributeType instanceof mendixmodelsdk_1.domainmodels.StringAttributeType) {
-                        attributeType.length = attributeData.Length ? parseInt(attributeData.Length, 10) : 50;
+        const entitiesMap = {};
+        jsonData.forEach(entityData => {
+            let entity = domainModel.entities.find(e => e.name === entityData.Name);
+            if (!entity) {
+                console.log(`Criando entidade "${entityData.Name}"`);
+                entity = mendixmodelsdk_1.domainmodels.Entity.createIn(domainModel);
+                entity.name = entityData.Name;
+                entitiesMap[entityData.Name] = entity;
+                entityData.Attributes.forEach((attributeData) => {
+                    if (attributeData.Attribute !== "Id") {
+                        const attribute = mendixmodelsdk_1.domainmodels.Attribute.createIn(entity);
+                        attribute.name = attributeData.Attribute;
+                        attribute.type = getDataType(attributeData.Type);
+                        if (attribute.type instanceof mendixmodelsdk_1.domainmodels.StringAttributeType) {
+                            attribute.type.length = attributeData.Length ? parseInt(attributeData.Length, 10) : 50;
+                        }
+                        else if (attribute.type instanceof mendixmodelsdk_1.domainmodels.DecimalAttributeType) {
+                            mendixmodelsdk_1.domainmodels.StoredValue.createIn(attribute).defaultValue = attributeData.Decimals ? attributeData.Decimals.toString() : "0";
+                        }
                     }
-                    else if (attributeType instanceof mendixmodelsdk_1.domainmodels.DecimalAttributeType) {
-                        mendixmodelsdk_1.domainmodels.StoredValue.createIn(attribute).defaultValue = attributeData.Decimals ? parseInt(attributeData.Decimals, 10).toString() : "10";
-                    }
-                    /*for (const indexData of entityData.Indexes) {
-                      const indexName = indexData.Name;
-                      const index = domainmodels.Index.createIn(entity);
-                      const indexx = domainmodels.IndexedAttribute.createIn(index);
-                      indexx.attribute = indexName;
-                    }*/
-                }
+                });
             }
-        }
+        });
+        jsonData.forEach(entityData => {
+            const entity = entitiesMap[entityData.Name];
+            entityData.Attributes.forEach((attributeData) => {
+                if (attributeData.IsIdentifier) {
+                    const referencedEntityName = attributeData.Type.replace(" Identifier", "");
+                    const referencedEntity = entitiesMap[referencedEntityName];
+                    if (referencedEntity) {
+                        console.log(`Criando relação 1-1 entre ${entity.name} e ${referencedEntity.name}`);
+                        const association1to1 = mendixmodelsdk_1.domainmodels.Association.createIn(domainModel);
+                        association1to1.name = `${referencedEntity.name}_${entity.name}`;
+                        association1to1.parent = entity;
+                        association1to1.child = referencedEntity;
+                        association1to1.type = mendixmodelsdk_1.domainmodels.AssociationType.Reference;
+                        association1to1.owner = mendixmodelsdk_1.domainmodels.AssociationOwner.Both;
+                    }
+                    else {
+                        console.log(`Entidade referenciada "${referencedEntityName}" não encontrada para o atributo "Id" em "${entity.name}"`);
+                    }
+                }
+            });
+        });
+        jsonData.forEach((entityData) => {
+            const entity = domainModel.entities.find((e) => e.name === entityData.Name);
+            if (entity && entityData.Indexes) {
+                entityData.Indexes.forEach((indexData) => {
+                    if (indexData.Name.toLowerCase() === "autoindex_id") {
+                        return;
+                    }
+                    const index = mendixmodelsdk_1.domainmodels.Index.createIn(entity);
+                    const attributeName = translateIndexName(indexData.Name);
+                    const attributeToIndex = entity.attributes.find((attr) => translateIndexName(attr.name) === attributeName);
+                    if (attributeToIndex) {
+                        const indexedAttribute = mendixmodelsdk_1.domainmodels.IndexedAttribute.createIn(index);
+                        indexedAttribute.attribute = attributeToIndex;
+                    }
+                    else {
+                        console.log(`Atributo para índice ${indexData.Name} não encontrado em ${entityData.Name}.`);
+                    }
+                });
+            }
+        });
         function getDataType(dataType) {
             switch (dataType.toLowerCase()) {
                 case "text":
@@ -65,6 +103,14 @@ async function executeScript(jsonData) {
                 default:
                     return mendixmodelsdk_1.domainmodels.StringAttributeType.create(model);
             }
+        }
+        function translateIndexName(indexOrAttributeName) {
+            const lowerCasedName = indexOrAttributeName.toLowerCase();
+            const translations = {
+                title: "Titulo",
+                name: "Nome",
+            };
+            return translations[lowerCasedName] || indexOrAttributeName;
         }
         await model.flushChanges();
         await workingCopy.commitToRepository("main");
